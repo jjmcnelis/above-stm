a2befb7e9ea550b4acf5b2b79c68efb1fc0176e1
McNelis, John J <mcnelisjj@ornl.gov>
2019-05-10 17:26:26 -0400

    version1

--- ABoVE.py
+++ ABoVE.py
@@ -1,103 +1,223 @@
 import json
-import ipyleaflet as pl
-import ipywidgets as pw
+import pandas as pd
 
-# ----------------------------------------------------------------------------
-# the above domain
+from shapely.geometry import shape
+from shapely.ops import cascaded_union
 
-domain_json = 'data/ABoVE_Study_Domain/ABoVE_Study_Domain.json'  
-with open(domain_json, 'r') as file:
-    above_domain = json.load(file)
-    above_domain["features"][0]["properties"]["style"] = {
-        "weight": 0.75,
-        "color": "#FFFFFF",
-        "fillColor": "#FFFFFF",
-        "fillOpacity": 0}
-    domain_layer = pl.GeoJSON(data=above_domain)
+from ipyleaflet import Map,LayerGroup,DrawControl,GeoJSON,basemaps,basemap_to_tiles,Polygon
+from ipywidgets import HTML,Layout,HBox,VBox,Textarea,Output
+
+from Query import *
 
 # ---------------------------------------------------------------------------- 
-# the above grid (not using C because too fine)
+# statics
 
-grid_json = ("data/ABoVE_240m_30m_5m_grid_tiles/"
-            "ABoVE_240m_30m_5m_grid_tiles.json")
-
-gridA, gridB = [], []
-with open(grid_json, 'r') as file:
+# the above grid; make layers later
+gridf = "data/ABoVE_240m_30m_5m_grid_tiles/ABoVE_240m_30m_5m_grid_tiles.json"
+with open(gridf, 'r') as file:
     above_grid = json.load(file)
-    
-    for feat in above_grid["features"]:
-        prop = feat["properties"]
-        level = prop["grid_level"]
-        prop = prop.update({
-            "style": {
-                "weight": 0.75,
-                "color": "aliceblue",
-                "fillOpacity": 0}})
-        
-        if level in ["A", "B"]:
-            
-            cell = pl.GeoJSON(
-                data=feat,
-                hover_style = {
-                    "weight": 1,
-                    "color": "aliceblue",
-                    "fillColor": "#FFFFFF",
-                    "fillOpacity": 0.4})       # make geojson map layer
 
-            if level=="A": 
-                gridA.append(cell)
-            if level=="B": 
-                gridB.append(cell)
+# the above domain; make layer now
+domainf = 'data/ABoVE_Study_Domain/ABoVE_Study_Domain.json'  
+with open(domainf, 'r') as file:
+    above_domain = json.load(file)
+
+# get some info about ABoVE collection in CMR for ORNL
+ornl_above = query_ornl_projects()
+
+# load a basemap from ESRI #basemaps.NASAGIBS.ModisTerraTrueColorCR
+esri = basemap_to_tiles(basemaps.Esri.WorldImagery)
+
+# map draw poly styling
+draw_style = {
+    "shapeOptions": {
+        "fillColor": "lightgreen",
+        "color": "lightgreen",
+        "fillOpacity": 0.5}}
 
 # ----------------------------------------------------------------------------
-# map widget interface
+# JSON input interface and some other ui elements
 
-# load a basemap layer from ESRI service
-esri = pl.basemap_to_tiles(pl.basemaps.Esri.WorldImagery)
-#gibs = pl.basemap_to_tiles(pl.basemaps.NASAGIBS.ModisTerraTrueColorCR, "2019-01-01")
-
-gridA_layer = pl.LayerGroup(layers=tuple(gridA))
-gridB_layer = pl.LayerGroup(layers=tuple(gridB))
-
-# initialize map widget
-m = pl.Map(
-    layers=(esri, gridB_layer, ),
-    center=(60, -135), 
-    zoom=2, 
-    scroll_wheel_zoom=True)
-
-#m.add_control(pl.LayersControl())
-
-# ----------------------------------------------------------------------------
-# JSON input interface
-
-# the default json string for selecting collections/granules 
-with open('data/geom.json', 'r') as file:
-    default_json = file.read().replace('\n', '')
-
-geojson_label = pw.HTML("<h4><b>Paste your GeoJSON: </b></h4>")
-geojson_text = pw.Textarea(
-    value=default_json,
+geojson_label = HTML("<h4><b> or paste your GeoJSON: </b></h4>")
+geojson_text = Textarea(
     placeholder='Paste GeoJSON string here.',
     disabled=False,
-    layout=pw.Layout(width="auto", height="200px"))
+    layout=Layout(width="50%", height="200px"))
+    
+above_domain["features"][0]["properties"]["style"] = {
+    "weight": 0.75,
+    "color": "#FFFFFF",
+    "fillColor": "#FFFFFF",
+    "fillOpacity": 0}
+domain_layer = GeoJSON(data=above_domain)
+
+header = HTML(
+    "<h4><b>Draw a polygon on the map or paste your GeoJSON: </b></h4>")
+
+instruct =  HTML(
+    "<p><b>Instructions:</p><p><b>1.<br>2.<br>3.<br>...</p>",
+    layout=Layout(width="50%"))
 
 
-# -------------------------------------------------------------------------------
-# map draw controls
+# ---------------------------------------------------------------------------- 
+# class to manage toggled grid cells
 
-draw_control = pw.DrawControl()
-draw_control.polyline =  {}
-draw_control.polygon = {}
-draw_control.circle = {}
-draw_control.rectangle = {
-    "shapeOptions": {
-        "fillColor": "#fca45d",
-        "color": "#fca45d",
-        "fillOpacity": 1.0}}
-m.add_control(draw_control)
 
-# -------------------------------------------------------------------------------
-# display!
+class Cell(object):
+    """ """
 
-display(pw.VBox([m, geojson_label, geojson_text]))
+    offstyle = {"fill_opacity": 0, "color": "white", "weight": 0.75}
+    onstyle = {"fill_opacity": 0.4, "color": "lightgreen", "weight": 1}
+
+    def __init__(self, feat):
+        """Inits with id,lat,lon; makes request string, map point."""
+
+        self.feat = feat
+        self.shape = shape(feat["geometry"])
+
+        self.prop = feat["properties"]
+        self.feat["properties"]["style"] = {
+            "fill_opacity": 0., 
+            "color": "white", 
+            "weight": 0.75}
+        self.id = self.prop["grid_id"]
+        self.level = self.prop["grid_level"]
+        
+        self.layer = GeoJSON(
+            data=self.feat,
+            hover_style = {
+                "weight": 1, 
+                "color": "white",
+                "fillColor": "white",
+                "fillOpacity": 0.8})
+        self.layer.on_click(self.toggle)
+        self.on = False
+
+    def toggle(self, **kwargs):
+        """Routine for when a cell is toggled on."""
+        self.on = False if self.on else True
+        
+
+class App():
+    """ """
+    
+    settings = {"enabled_grid": "B"}
+
+
+    def __init__(self, session=None):
+
+        self.session = session
+        self.use_grid = self.settings["enabled_grid"]
+
+        # generate map grid polygon layers
+        self.grid_layers = LayerGroup()
+        self.grid_dict = {}
+        
+        for feat in above_grid["features"]:
+            level = feat["properties"]["grid_level"]
+            if level==self.use_grid:
+                Cell_object = Cell(feat) 
+                #Cell_object.layer.on_click()
+
+                grid_id = Cell_object.id
+                self.grid_dict[grid_id] = Cell_object
+                self.grid_layers.add_layer(self.grid_dict[grid_id].layer)
+        
+        # make an attribute that will hold selected layer
+        self.selected_layer = LayerGroup()
+
+        self.map = Map(
+            layers=(esri, self.grid_layers, self.selected_layer, ),
+            center=(65, -100), 
+            zoom=2, 
+            width="auto", 
+            height="auto",
+            scroll_wheel_zoom=True)
+
+        # map draw controls
+        self.draw_control = DrawControl()
+        self.draw_control.polyline =  {}
+        self.draw_control.circle = {}
+        self.draw_control.circlemarker = {}
+        self.draw_control.remove = False
+        self.draw_control.edit = False
+        self.draw_control.polygon = {**draw_style}
+        self.draw_control.rectangle = {**draw_style}
+        self.draw_control.on_draw(self.update_selected_cells)
+        self.map.add_control(self.draw_control)
+
+        
+        # output display
+        self.output = Output(layout=Layout(width="auto"))
+        
+        # make the widget layout
+        self.ui = VBox([
+            header, 
+            HBox([instruct, geojson_text]),
+            self.map,
+            self.output
+        ], layout=Layout(width="auto"))
+
+        # display ui
+        display(self.ui)
+
+
+    # ------------------------------------------------------------------------
+
+
+    def update_selected_cells(self, *args, **kwargs):
+        """ """
+        # clear all draw and selection layers
+        self.draw_control.clear()
+        
+        # --------------------------------------------------------------------
+        # update active cells and make a big merged polgyon for selection
+
+        # make shapely geom from geojson 
+        drawn_json = kwargs["geo_json"]
+        shapely_geom = shape(drawn_json["geometry"])
+
+        # iterate over cells and toggle selected
+        cells = self.grid_dict
+        on = []
+        for id, cell in cells.items():
+            
+            # if drawn_geom intersects cell.shape, toggle
+            if shapely_geom.intersects(cell.shape):
+                #cell.toggle()                                              # no toggling for now, only pick select
+                #cells[id] = cell
+                on.append(cell.shape)
+        
+        # get the union of all of the cells that are toggled on
+        #union = cascaded_union([c.shape for c in cells.values() if c.on])  # no toggling for now, only pick select
+        union = cascaded_union(on)
+
+        # make layer that represents selected cells and add to selected_layer
+        self.selected_layer.clear_layers()
+        x,y = union.exterior.coords.xy
+        self.selected_layer.add_layer(Polygon(locations=list(zip(y,x))))
+        
+        # --------------------------------------------------------------------
+        # find all CMR collections that intersect with merged cells geom
+
+        selected = []
+        for index, collection in ornl_above.iterrows():
+            
+            box = collection.boxes
+            shapely_box = CMR_box_to_Shapely_box(box[0])
+            
+            # intersect: use shapely_geom if strictly using drawn poly
+            intersect_bool = shapely_box.intersects(union) 
+            if intersect_bool:
+                selected.append(index)
+
+        selected_collections = ornl_above.iloc[selected]
+        with self.output:
+            display(selected_collections)
+
+
+##############################################################################
+# launch it braj!
+##############################################################################
+
+app = App()
